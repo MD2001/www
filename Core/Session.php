f03c49d3d33630ffb5886b0d24e9654c5d4b0d99
Mohamed Diaa <mdiea0581@gmail.com>
2024-06-09 15:19:46 +0300

    Session class v0.2

--- Core/Session.php
+++ Core/Session.php
@@ -4,22 +4,39 @@ namespace Core;
 
 class Session
 {
-    public static function put($key,$value)
+    public static function put($key, $value)
     {
-        $_SESSION[$key]=$value;
-    } 
-    public static function get($key,$defult=null)
+        $_SESSION[$key] = $value;
+    }
+    public static function get($key, $defult = null)
     {
-       return $_SESSION["_flash"][$key] ?? $_SESSION[$key] ?? $defult;
-    } 
+        return $_SESSION["_flash"][$key] ?? $_SESSION[$key] ?? $defult;
+    }
     public static function have($key)
     {
         return (bool)  static::get($key);
-    } 
-    public static function flash($key,$value)
+    }
+    public static function flash($key, $value)
     {
-        $_SESSION["_flash"][$key]=$value;
-    } 
-    
-    
+        $_SESSION["_flash"][$key] = $value;
+    }
+
+    public static function unflash()
+    {
+        unset($_SESSION["_flash"]);
+    }
+
+    public static function flush()
+    {
+        $_SESSION = [];
+    }
+
+    public static function destroy()
+    {
+        $_SESSION = [];
+        session_destroy();
+
+        $params = session_get_cookie_params();
+        setcookie("PHPSESSID", "", time() - 3600, $params['path'], $params['domain'], $params['secure '], $params['httponly']);
+    }
 }
--- Core/functions.php
+++ Core/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Response;
+use Core\Session;
 
 function abort($code = 404)
 {
@@ -9,25 +10,24 @@ function abort($code = 404)
     die();
 }
 
-function dd ($Copy_var)
+function dd($Copy_var)
 {
 
     echo "<pre>";
     var_dump($Copy_var);
     echo "</pre>";
     die();
-
 }
 
 function urlis($url)
 {
-    return $_SERVER["REQUEST_URI"]=== $url;
+    return $_SERVER["REQUEST_URI"] === $url;
 }
 
 
-function athrauzation($cindition,$states=Response::UNOTHRAIZED)
+function athrauzation($cindition, $states = Response::UNOTHRAIZED)
 {
-    if(!$cindition){
+    if (!$cindition) {
 
         abort($states);
     }
@@ -35,12 +35,12 @@ function athrauzation($cindition,$states=Response::UNOTHRAIZED)
 
 function base_path($path)
 {
-    return BaseBase.$path;
+    return BaseBase . $path;
 }
-function view($path,$arrtipute=[])
+function view($path, $arrtipute = [])
 {
     extract($arrtipute);
-    require  base_path("Views/" . $path );
+    require  base_path("Views/" . $path);
 }
 
 function base_path_requier($path)
@@ -48,27 +48,28 @@ function base_path_requier($path)
     return require  base_path($path);
 }
 
-function generateRandomString($length = 16) {
+function generateRandomString($length = 16)
+{
     // Define character set
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
-    
+
     for ($i = 0; $i < $length; $i++) {
         // Append a random character from the character set
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
-    
+
     return $randomString;
 }
 
 function login($arr)
 {
-    $_SESSION["logging_in"]=true;
-    $_SESSION["user_info"]=[
-        "email"=>$arr["email"]??null,
-        "username" =>$arr["username"]??null,
-        "userid"=>$arr["id"]??null
+    $_SESSION["logging_in"] = true;
+    $_SESSION["user_info"] = [
+        "email" => $arr["email"] ?? null,
+        "username" => $arr["username"] ?? null,
+        "userid" => $arr["id"] ?? null
     ];
 
     session_regenerate_id(true);    //to not let user reuse the cocke saved in the serever
@@ -76,15 +77,10 @@ function login($arr)
 
 function logout()
 {
-    $_SESSION=[];
-session_destroy();
-
-$params=session_get_cookie_params();
-setcookie("PHPSESSID","", time()- 3600, $params['path'], $params['domain'], $params['secure '], $params['httponly']);
-
+    Session::destroy();
 }
 
-function redirect($location="/")
+function redirect($location = "/")
 {
     header("Location: {$location}");
     exit();
